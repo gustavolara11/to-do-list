6ba12534d44810e5be511e1b6c4b37655e9fa208
gustavolara11 <gustavex2007@gmail.com>
2023-05-01 12:36:38 -0300

    feat: fix input issues

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-const onKey = () => {
-  const input = document.querySelector("#to-do-input");
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("form");
+  const input = document.getElementById("to-do-input");
+  const table = document.getElementById("table");
 
-  input.addEventListener("keyup", function (event) {
-    if (event.keyCode === 13) {
-      const valorInput = input.value;
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-      const table = document.getElementById("table");
-      const novoInput = document.createElement("input");
-      novoInput.classList.add("to-do-list");
-      novoInput.value = valorInput;
-      const newRow = document.createElement("tr");
-      const newCell = document.createElement("td");
-      newCell.appendChild(novoInput);
-      newRow.appendChild(newCell);
-      table.appendChild(newRow);
+    const valorInput = input.value;
 
-      input.value = "";
-    }
+    const novoInput = document.createElement("input");
+    novoInput.classList.add("to-do-list");
+    novoInput.value = valorInput;
+    const newRow = document.createElement("tr");
+    const newCell = document.createElement("td");
+    newCell.appendChild(novoInput);
+    newRow.appendChild(newCell);
+    table.appendChild(newRow);
+
+    input.value = "";
   });
-};
+});
